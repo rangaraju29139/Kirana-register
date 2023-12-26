ed943e0751c2f593b11818b14d2972bf456be908
rangaraju penmetsa <rangaraju29139@gmail.com>
2023-12-27 01:15:10 +0530

    added starter validations and user entity

---
+++ src/main/java/com/example/kiranaregister/entities/User.java
@@ -0,0 +1,39 @@
+package com.example.kiranaregister.entities;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotNull
+    @Size(min=2)
+    private String firstName;
+    @NotNull
+    @Size(min=2)
+    private String lastName;
+
+    @NotNull
+    @Size(min=2)
+    @Email
+    private String email;
+
+    @NotNull
+    @Size(min=2)
+    private String password;
+
+    @NotNull
+    @Size(min=2)
+    @Pattern(regexp = "[0-9]{10}")
+    private String phoneNumber;
+
+}
