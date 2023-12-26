949a5fe238e0a5084a10caafcf68aa8abdd66aa3
rangaraju penmetsa <rangaraju29139@gmail.com>
2023-12-26 20:30:20 +0530

    initial commit

---
+++ src/test/java/com/example/kiranaregister/KiranaRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kiranaregister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiranaRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
