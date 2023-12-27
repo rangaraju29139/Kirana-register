15005c98bab7f4e6dd19ab8add4729981c11d16b
rangaraju penmetsa <rangaraju29139@gmail.com>
2023-12-27 14:43:05 +0530

    added the get Customer end point

--- src/main/java/com/example/kiranaregister/services/CustomerService.java
+++ src/main/java/com/example/kiranaregister/services/CustomerService.java
@@ -2,6 +2,9 @@ package com.example.kiranaregister.services;
 
 import com.example.kiranaregister.entities.Customer;
 
+import java.util.Optional;
+
 public interface CustomerService {
     public Customer createCustomer(Customer customer);
+    public Optional<Customer> getCustomer(Long id);
 }
