e04410e80a75d5991cef89297cb0a8c2d0e2f35b
rangaraju penmetsa <rangaraju29139@gmail.com>
2023-12-27 11:32:50 +0530

    added the transaction

--- src/main/java/com/example/kiranaregister/entities/Transaction.java
+++ src/main/java/com/example/kiranaregister/entities/Transaction.java
@@ -1,6 +1,10 @@
 package com.example.kiranaregister.entities;
 
+import com.example.kiranaregister.entities.enums.ConsumerType;
+import com.example.kiranaregister.entities.enums.PaymentType;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -17,5 +21,24 @@ public class Transaction {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @NotNull
+    @Size(min = 3)
+    private String description;
 
+    @ManyToOne
+    @NotNull
+    private Store store;
+
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private ConsumerType consumerType;
+
+    @NotNull
+    private Long consumerId;
+
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private PaymentType paymentType;
+
+    private String referenceId;
 }
